cf9d5f21bd004b3da810bfa82c3516afac0485c9
Becca <beccacauthorn@gmail.com>
2020-04-13 21:04:07 -0400

    assignment2classpractice

---
+++ Elephant_queries.py
@@ -0,0 +1,23 @@
+import os
+from dotenv import load_dotenv
+import psycopg2
+
+load_dotenv() #> loads contents of the .env file into the script's environment
+
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+
+connection = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASSWORD, host=DB_HOST)
+print(type(conn))
+
+### A "cursor", a structure to iterate over db records to perform queries
+cur = conn.cursor()
+### An example query
+cur.execute('SELECT * from test_table;')
+### Note - nothing happened yet! We need to actually *fetch* from the cursor
+results = cur.fetchone() 
+print(results)
+
+conn.close()
